d14ce3bae16cafbf12afdf10b54095b321aa2932
Fidel <fidel050987@gmail.com>
2026-04-22 18:27:13 -0600

    se implementó un límite en los controladores

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, UseGuards } from '@nestjs/common';
+import { Throttle, ThrottlerGuard } from '@nestjs/throttler';
 import { AuthService } from './auth.service';
 import { LoginDto } from '../auth/dto/login.dto';
 import { ForgotPasswordDto } from './dto/forgot-password.dto';
@@ -20,18 +21,24 @@ export class AuthController {
 
   @Public()
   @Post('forgot-password')
+  @UseGuards(ThrottlerGuard)
+  @Throttle({ default: { limit: 3, ttl: 60_000 } })
   forgotPass(@Body() forgotPasswordDto: ForgotPasswordDto) {
     return this.authService.forgotPass(forgotPasswordDto);
   }
 
   @Public()
   @Post('validate-token')
+  @UseGuards(ThrottlerGuard)
+  @Throttle({ default: { limit: 5, ttl: 60_000 } })
   validateToken(@Body() validateTokenDto: ValidateTokenDto) {
     return this.authService.validateToken(validateTokenDto);
   }
 
   @Public()
   @Post('reset-password/:token')
+  @UseGuards(ThrottlerGuard)
+  @Throttle({ default: { limit: 5, ttl: 60_000 } })
   resetPasswordWithToken(
     @Param('token') token: string,
     @Body() resetPassword: ResetPasswordDto,
